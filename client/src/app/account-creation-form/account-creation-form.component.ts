552d9de104b310440ac014df43791597102ca314
Zachary Klausner <zachklausner@gmail.com>
2021-09-14 20:30:42 -0600

    added custom validator for create account page confirm password field

--- client/src/app/account-creation-form/account-creation-form.component.ts
+++ client/src/app/account-creation-form/account-creation-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-account-creation-form',
@@ -22,11 +22,39 @@ export class AccountCreationFormComponent implements OnInit {
     Validators.pattern('[a-zA-Z0-9_]*')
   ]);
 
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(72)
+  ]);
+
+  confirmPasswordFormControl = new FormControl('', [
+    Validators.required,
+    this.confirmPasswordValidator(this.password)
+  ]);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit() { this.submitted=true;}
+
+  confirmPasswordValidator(pwd: String): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      console.log("control:" + control.value)
+      console.log("pwd" + pwd)
+      if (control.value === pwd) {
+        return {match: {value: false}}
+      }
+      else 
+        return null;
+    };
+  };
 
 }
